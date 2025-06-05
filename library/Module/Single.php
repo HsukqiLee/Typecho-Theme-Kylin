7a1fec48f8e85b429a6cd0442fe9d7d89cb7b27f
Hsukqi Lee <team@tsinbei.com>
2025-06-05 18:28:01 +0800

    Revert "Update Single.php"
    This reverts commit 35f300d8e758239878b8ef43ccba359c87adf25f.

--- library/Module/Single.php
+++ library/Module/Single.php
@@ -101,14 +101,13 @@ class WDCX_Module_Single
             <div class="level-start">
                 <div class="level-item">
                     <span class="is-size-6 has-text-grey has-mr-7">#</span>
-                    <span><?php $result = array();
+                    <?php $result = array();
                     foreach ($this->_post->tags as $tag) {
                         $result[] ='<a class="has-link-grey" href="' . $tag['permalink'] . '">'
                         . $tag['name'] . '</a>';
                     }
                     echo implode(',&nbsp;', $result);
                     ?>
-                    </span>
                 </div>
             </div>
         </div>
